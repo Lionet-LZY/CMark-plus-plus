fad40704410e2aa7cfba57da7c24c8a423e208cf
Tingrui Li <muting3298811877@163.com>
2025-05-19 23:05:03 +0800

    完善块元素语法定义

--- Markdown_BlockElement.h
+++ Markdown_BlockElement.h
@@ -1,1 +1,21 @@
+// 块元素 Markdown 语法
+
 #pragma once
+#include <string>
+//枚举语法类型
+enum class BlockType {
+	Paragraph,
+	Headinglevel1,
+	Headinglevel2,
+	Headinglevel3,
+	HorizontalRules
+};
+class Markdown_BlockElement {
+private:
+	BlockType type;
+	std::string text;
+public:
+	Markdown_BlockElement(BlockType T, const std::string& t);
+	BlockType getType()const;
+	const std::string& getText()const;
+};
