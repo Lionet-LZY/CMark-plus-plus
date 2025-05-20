a0eed69b0c6f76efae64ac22f9b2bb768176d2cd
Tingrui Li <muting3298811877@163.com>
2025-05-20 10:25:51 +0800

    删除段元素定义与实现

--- Markdown_ParaElement.cpp
+++
@@ -1,9 +0,0 @@
-//块元素Markdown语法实现
-#include"Markdown_ParaElement.h"
-Markdown_ParaElement::Markdown_ParaElement(ParaType T,const std::string&t):type(T),text(t){}
-ParaType Markdown_ParaElement::getType()const {
-	return type;
-}
-const std::string& Markdown_ParaElement::getText()const {
-	return text;
-}
